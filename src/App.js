6509b5518dc7e78b8ce1c2fbfd03c638367a14dc
Trunova Yulia <103053349+Yukkaf1@users.noreply.github.com>
2023-04-12 19:40:30 +0200

    dogs

--- src/App.js
+++ src/App.js
@@ -1,30 +1,49 @@
 import { Component } from 'react';
-
-import { ToastContainer } from 'react-toastify';
-import PokemonForm from './components/PokemonForm';
-import PokemonInfo from './components/PokemonInfo';
+import { GlobalStyle } from './components/GlobalStyle';
+import axios from 'axios';
+import Select from 'react-select';
+axios.defaults.baseURL = 'https://api.thedogapi.com/v1';
+axios.defaults.headers.common['x-api-key'] = process.env.REACT_APP_API_KEY;
 
 export default class App extends Component {
   state = {
-    pokemonName: '',
+    breeds: [],
+    dog: null,
   };
 
-  //   componentDidMount() {
-  //     fetch('https://pokeapi.co/api/v2/pokemon/ditto')
-  //       .then(res => res.json())
-  //       .then(this.handleFormSubmit);
-  //   }
+  async componentDidMount() {
+    try {
+      const response = await axios.get('/breeds');
+      this.setState({ breeds: response.data });
+    } catch (error) {}
+  }
 
-  handleFormSubmit = pokemonName => {
-    this.setState({ pokemonName });
+  selectBreed = async option => {
+    console.log(option);
+    try {
+      const response = await axios.get('/images/search', {
+        params: { breed_id: option.value },
+      });
+      this.setState({ dog: response.data[0] });
+    } catch (error) {}
+  };
+
+  buildSelectOptions = () => {
+    return this.state.breeds.map(breed => ({
+      value: breed.id,
+      label: breed.name,
+    }));
   };
 
   render() {
+    const options = this.buildSelectOptions();
     return (
-      <div style={{ maxWidth: 1170, margin: '0 auto', padding: 20 }}>
-        <PokemonForm onSubmit={this.handleFormSubmit} />
-        <PokemonInfo pokemonName={this.state.pokemonName} />
-        <ToastContainer autoClose={3000} />
+      <div>
+        <Select options={options} onChange={this.selectBreed} />
+        {this.state.dog && (
+          <img src={this.state.dog.url} width="480" alt="dog" />
+        )}
+        <GlobalStyle />
       </div>
     );
   }
