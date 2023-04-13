2126c3bd273b21738bde2a01156aaa5c2135e05f
Trunova Yulia <103053349+Yukkaf1@users.noreply.github.com>
2023-04-13 17:17:52 +0200

    dogs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import { Component } from 'react';
 import { GlobalStyle } from './components/GlobalStyle';
 import axios from 'axios';
-
+import ClipLoader from 'react-spinners/ClipLoader';
 import { fetchBreeds, fetchDogByBreed } from 'api';
 import { Dog } from './components/Dog';
 import { BreedSelect } from 'components/BreedSelect';
+
 axios.defaults.baseURL = 'https://api.thedogapi.com/v1';
 axios.defaults.headers.common['x-api-key'] = process.env.REACT_APP_API_KEY;
 
@@ -13,14 +14,18 @@ export default class App extends Component {
     breeds: [],
     dog: null,
     error: null,
+    isLoading: false,
   };
 
   selectBreed = async breedId => {
     try {
+      this.setState({ isLoading: true });
       const dog = await fetchDogByBreed(breedId);
       this.setState({ dog });
     } catch (error) {
       this.setState({ error: 'Ошибка загрузки Попробуй еще раз!' });
+    } finally {
+      this.setState({ isLoading: false });
     }
   };
 
@@ -32,13 +37,14 @@ export default class App extends Component {
   };
 
   render() {
-    const { breeds, dog, error } = this.state;
+    const { breeds, dog, error, isLoading } = this.state;
     const options = this.buildSelectOptions();
     return (
       <div>
         <BreedSelect breeds={breeds} onSelect={this.selectBreed} />
         {error && <div> {error} </div>}
-        {dog && <Dog dog={dog} />}
+        {isLoading && <ClipLoader color="#36d7b7" />}
+        {dog && !isLoading && <Dog dog={dog} />}
         <GlobalStyle />
       </div>
     );
